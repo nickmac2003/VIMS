261c6bb62a9cff8af161489c3f9419a8d10b0aef
nickmac2003 <82114957+nickmac2003@users.noreply.github.com>
2022-02-14 18:33:18 -0600

    Cleanup

---
+++ VIMS Website/app/public/config.php
@@ -0,0 +1,15 @@
+<?php 
+
+/* Database credentials */
+$servername = "authentication-database.cvczlcshulp9.us-east-2.rds.amazonaws.com:3306";
+$username = "admin";
+$password = "password";
+$dbname = "Authentication_Database";
+$con = new mysqli($servername, $username, $password, $dbname);
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+}
+
+//$con->close();
+
+?>
