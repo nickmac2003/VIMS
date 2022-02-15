62525aed18a945afd3294489d630a94298a6c545
nickmac2003 <82114957+nickmac2003@users.noreply.github.com>
2022-02-14 20:52:49 -0600

    vims

---
+++ vims_website/app/public/connect_db.php
@@ -0,0 +1,17 @@
+<?php
+$servername='machines-database.cvczlcshulp9.us-east-2.rds.amazonaws.com:3306';
+$username="admin";
+$password="password";
+ 
+try
+{
+    $con=new PDO("mysql:host=$servername;dbname=machines_database",$username,$password);
+    $con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    //echo 'connected';
+}
+catch(PDOException $e)
+{
+    echo '<br>'.$e->getMessage();
+}
+     
+?>
