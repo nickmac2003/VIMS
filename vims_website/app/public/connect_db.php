0c9b63b6dea6db6e253a95a7cc550a1a65cc1e2b
nickmac2003 <82114957+nickmac2003@users.noreply.github.com>
2022-03-06 21:15:11 -0600

    Update connect_db.php

--- vims_website/app/public/connect_db.php
+++ vims_website/app/public/connect_db.php
@@ -1,17 +1,17 @@
 <?php
-$serverName='machines-database.cvczlcshulp9.us-east-2.rds.amazonaws.com:3306';
+$servername='machines-database.cvczlcshulp9.us-east-2.rds.amazonaws.com:3306';
 $username="admin";
 $password="password";
- 
+
 try
 {
-    $con=new PDO("mysql:host=$serverName;dbname=machines_database",$username,$password);
+    $con = new PDO("mysql:host=$servername;dbname=machines_database",$username,$password);
     $con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    //echo 'connected';
+    echo 'Database Connected<br>';
 }
 catch(PDOException $e)
 {
     echo '<br>'.$e->getMessage();
 }
-     
+
 ?>
