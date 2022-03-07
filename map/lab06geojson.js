90ad5d6cab24d7f60a6598b410095e9737071ba3
nickmac2003 <82114957+nickmac2003@users.noreply.github.com>
2022-03-06 21:42:04 -0600

    Create lab06geojson.js

---
+++ map/lab06geojson.js
@@ -0,0 +1,15 @@
+var mydata = {"type": "FeatureCollection", "features": [
+
+{"type": "Feature", "geometry": {"type": "Point", "coordinates": [-94.72606973064954, 38.899330072668214]}, "properties": {"Machine ID": "100001", "Pepsi": 19, "Diet P$
+
+{"type": "Feature", "geometry": {"type": "Point", "coordinates": [-94.67371428646524, 38.96337655008801]}, "properties": {"Machine ID": "100002", "Pepsi": 16, "Diet Pe$
+
+{"type": "Feature", "geometry": {"type": "Point", "coordinates": [-94.70676933064641, 39.01610579921262]}, "properties": {"Machine ID": "100003", "Pepsi": 21, "Diet Pe$
+
+{"type": "Feature", "geometry": {"type": "Point", "coordinates": [-94.76168401530256, 38.93306961525438]}, "properties": {"Machine ID": "100004", "Pepsi": 20, "Diet Pe$
+
+{"type": "Feature", "geometry": {"type": "Point", "coordinates": [-95.25889128832105, 38.95427914784963]}, "properties": {"Machine ID": "100005", "Pepsi": 15, "Diet Pe$
+
+{"type": "Feature", "geometry": {"type": "Point", "coordinates": [-94.68939425762704, 39.0249677229763]}, "properties": {"Machine ID": "100006", "Pepsi": 13, "Diet Pep$
+
+]}
