9b04e9e02a9fea4d9c9eabe5f4a8756ae7598649
Ubuntu <ubuntu@ip-172-31-23-41.us-east-2.compute.internal>
2022-04-27 03:48:54 +0000

    modified:   search.php

--- vims_website/app/public/search.php
+++ vims_website/app/public/search.php
@@ -1,5 +1,5 @@
 <?php
-// Check if the user is already logged in, if yes then redirect them to the sea$
+// Check if the user is already logged in, if yes then redirect them to the search page
 if(!isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     header("location: index.php");
     exit;
@@ -16,24 +16,20 @@ if(isset($_GET['save'])) {
         $stmt->execute();
         $inventory_results = $stmt->fetchAll(PDO::FETCH_ASSOC);
         //print_r($inventory_results);
-                $stmt2 = $con->prepare("SELECT maintenance_date FROM machines_database.machine WHERE machine_id =$search ");
+		$stmt2 = $con->prepare("SELECT maintenance_date FROM machines_database.machine WHERE machine_id =$search ");
         $stmt2->execute();
         $maintenance_date = $stmt2->fetchAll(PDO::FETCH_ASSOC);
-                //print_r($maintenance_date);
-                $stmt3 = $con->prepare("SELECT * FROM machines_database.money WHERE machine_id =$search ");
+		//print_r($maintenance_date);
+		$stmt3 = $con->prepare("SELECT * FROM machines_database.money WHERE machine_id =$search ");
         $stmt3->execute();
         $cash_in_machine = $stmt3->fetchAll(PDO::FETCH_ASSOC);
-                //print_r($cash_in_machine);
+		//print_r($cash_in_machine);
     }
     else {
         $searchErr = "Please Enter a Six Digit Machine ID";
     }
 }
 ?>
-<<<<<<< HEAD
-=======
-
->>>>>>> c91d0939161406df5119109d9e6395dcacf68580
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -63,7 +59,6 @@ th, td {
 </head>
 <body>
     <div id="id01" class="modal">
-<<<<<<< HEAD
     	<form class="modal-content animate" >
         	<div class="imgcontainer">
             		<img src="images/vims.PNG" alt="logo" class="logo">
@@ -177,131 +172,6 @@ th, td {
 		       		</table>
                        </div>
 		</div>
-=======
-        <form class="modal-content animate" >
-                <div class="imgcontainer">
-                        <img src="images/vims.PNG" alt="logo" class="logo">
-                </div>
-                <div class="container">
-                        <br/><br/>
-                        <form class="form-horizontal" action="#" method="get">
-                        <div class="row" style="margin-top: -40px">
-                                <div class="form-group">
-                                        <label class="control-label col-sm-4" form="machine_id"><b>Machine ID :</b></label>
-                                        <div class="col-sm-4">
-                                                <input type="text" class="form-control" name="search" placeholder="Enter a Six-Digit Machine ID">
-                                         </div>
-                                         <div class="col-sm-2">
-                                                <button type="submit" name="save" class="btn btn-success btn-sm">Submit</button>
-                                                <button type="button" onclick="location.href='VIMS-Map.html'" class="cancelbtn">Map</button>
-                                         </div>
-                                </div>
-                                <div class="form-group">
-                                        <span class="error" style="color:red;"> <?php echo $searchErr;?> </span>
-                                </div>
-                        </div>
-                        </form>
-                        <br/><br/>
-                            <h3 style="margin-top: -20px"><u>Machine Information:</u></h3><br/>
-                        <div class="table-responsive">
-			 <table class="table" style="font-size: 14px">
-                                <thead>
-                                        <tr>
-                                                <th>  Machine ID  </th>
-                                                <th>  Pepsi  </th>
-                                                <th>  Diet Pepsi  </th>
-                                                <th>  Dr Pepper  </th>
-                                                <th>  Mt Dew  </th>
-                                                <th>  Sierra Mist  </th>
-                                        </tr>
-                                </thead>
-                                <tbody>
-                                        <?php
-                                        if(!$inventory_results) {
-                                        echo '<tr>No data found</tr>';
-                                        }
-                                        else{
-                                                foreach($inventory_results as $key=>$value)
-                                        {
-                                        ?>
-                                        <tr>
-                                                <td><?php echo $value['machine_id'];?></td>
-                                                <td><?php echo $value['pe_qty'];?></td>
-                                                <td><?php echo $value['dp_qty'];?></td>
-                                                <td><?php echo $value['dr_qty'];?></td>
-                                                <td><?php echo $value['md_qty'];?></td>
-                                                <td><?php echo $value['sm_qty'];?></td>
-					 </tr>
-                                        <?php
-                                        }
-                                        }
-                                        ?>
-                                </tbody>
-                                </table>
-                                </br>
-                                <table class="table" style="font-size: 14px" >
-                                <thead>
-                                        <tr>
-                                                <th>Fives</th>
-                                                <th>Dollars</th>
-                                                <th>Quarters</th>
-                                                <th>Dimes</th>
-                                                <th>Nickels</th>
-                                                <th>Machine Total</th>
-                                        </tr>
-                                </thead>
-                                <tbody>
-				 <?php
-                                        if(!$cash_in_machine) {
-                                        echo '<tr>No data found</tr>';
-                                        }
-                                        else{
-                                                foreach($cash_in_machine as $key=>$value)
-                                        {
-                                        ?>
-                                        <tr>
-                                                <td><?php echo $value['fives_qty'];?></td>
-                                                <td><?php echo $value['dollars_qty'];?></td>
-                                                <td><?php echo $value['quarters_qty'];?></td>
-                                                <td><?php echo $value['dimes_qty'];?></td>
-                                                <td><?php echo $value['nickels_qty'];?></td>
-                                                <td><?php echo "$".$value['machine_total'];?></td>
-                                                </tr>
-                                        <?php
-                                        }
-                                        }
-                                        ?>
-                                </tbody>
-                                </table>
-                                </br>
-                                <table class="table" style="font-size: 14px" >
-                                <thead>
-                                        <tr>
-                                                <th>Maintenance Date</th>
-                                        </tr>
-                                </thead>
-                                <tbody>
-				 <?php
-                                if(!$maintenance_date) {
-                                        echo '<tr>No data found</tr>';
-                                }
-                                else{
-                                        foreach($maintenance_date as $key=>$value)
-                                {
-                                ?>
-                                        <tr>
-                                                <td><?php echo $value['maintenance_date'];?></td>
-
-                                        </tr>
-                                <?php
-                                }
-                                }
-                                ?>
-                                </tbody>
-                                </table>
-                       </div>
-                </div>
->>>>>>> c91d0939161406df5119109d9e6395dcacf68580
         </form>
     </div>
 </body>
