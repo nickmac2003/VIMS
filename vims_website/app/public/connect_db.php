0addf8a4421a9089a4bc564256f5a40f9b81da32
nickmac2003 <82114957+nickmac2003@users.noreply.github.com>
2022-04-27 01:03:52 -0500

    Update connect_db.php

--- vims_website/app/public/connect_db.php
+++ vims_website/app/public/connect_db.php
@@ -7,20 +7,12 @@ try
 {
     $con = new PDO("mysql:host=$servername;dbname=machines_database",$username,$password);
     $con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-<<<<<<< HEAD
    // echo 'Database Connected<br>';
 }  
-=======
     echo 'Database Connected<br>';
 }
->>>>>>> c91d0939161406df5119109d9e6395dcacf68580
 catch(PDOException $e)
 {
     echo '<br>'.$e->getMessage();
 }
-<<<<<<< HEAD
-     
-=======
-
->>>>>>> c91d0939161406df5119109d9e6395dcacf68580
 ?>
